7a52cf21934411d17c0d8c9dc6ae9118c6c62cdb
friendly-flame <armsnyder@gmail.com>
2015-12-07 15:08:47 -0600

    6A

---
+++ boosting.py
@@ -0,0 +1,56 @@
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import confusion_matrix
+import pickle
+import numpy as np
+from sklearn import svm
+
+
+def boosting_A(training_set, training_labels, testing_set, testing_labels):
+    # Build boosting algorithm for question A
+    # Return confusion matrix
+    classifier = AdaBoostClassifier()
+    classifier.fit(training_set, training_labels)
+    predicted_labels = classifier.predict(testing_set)
+    print 'error:', error_measure(predicted_labels, testing_labels)
+    return confusion_matrix(testing_labels, predicted_labels)
+
+
+def boosting_B(training_set, training_labels, testing_set, testing_labels):
+    # Build boosting algorithm for question B
+    # Return confusion matrix
+    base_classifier = classifier = svm.SVC(C=8, kernel='rbf', gamma=0.02)
+    classifier = AdaBoostClassifier(base_estimator=base_classifier, algorithm='SAMME')
+    classifier.fit(training_set, training_labels)
+    predicted_labels = classifier.predict(testing_set)
+    print 'error:', error_measure(predicted_labels, testing_labels)
+    return confusion_matrix(testing_labels, predicted_labels)
+
+
+def error_measure(predicted, actual):
+    return np.count_nonzero(abs(predicted - actual)) / float(len(predicted))
+
+
+def preprocess(images, binary=False):
+    # this function is suggested to help build your classifier.
+    # You might want to do something with the images before
+    # handing them to the classifier. Right now it does nothing.
+    if binary:
+        return np.array([x > 0.5 for x in [i.flatten() for i in images]], dtype=bool)
+    else:
+        return np.array([i.flatten() for i in images], dtype=float)
+
+
+def main():
+    training_set, training_labels, testing_set, testing_labels = (
+        pickle.load(open('training_set_2.p')),
+        pickle.load(open('training_labels_2.p')),
+        pickle.load(open('testing_set_2.p')),
+        pickle.load(open('testing_labels_2.p'))
+    )
+    training_set = preprocess(training_set, False)
+    testing_set = preprocess(testing_set, False)
+    print boosting_B(training_set, training_labels, testing_set, testing_labels)
+
+
+if __name__ == '__main__':
+    main()
